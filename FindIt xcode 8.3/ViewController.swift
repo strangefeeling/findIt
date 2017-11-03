d30eab05551b1daacfcf61de0a1d05d70f4a2348
rytis razmus <rytis@Rytiss-MacBook-Pro.local>
2017-11-03 11:52:27 +0200

    Initial Commit

---
+++ FindIt xcode 8.3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindIt xcode 8.3
+//
+//  Created by Rytis on 03/11/2017.
+//  Copyright © 2017 Rytis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
